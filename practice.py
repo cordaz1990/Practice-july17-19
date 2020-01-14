2252664c2d2770183f6cca80de47506b5fbc5953
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 20:28:35 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,21 @@
-print("hello")
-print("world"
+def countdown(n):
+    while n > 0:
+      print(n)
+      n = n - 1
+    print('Blastoff!')
+    
+def sequence(n):
+    while n != 1:
+       print(n)
+       if n % 2 == 0:
+          n = n / 2
+       else:
+          n = n * 3 + 1
+          
+while True:
+      line = input('> ')
+      if line == 'done':
+          break
+      print(line)
+      
+print('Done!')
